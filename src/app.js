6f4658ec58c8c05e537b230c5d4953e65f7df291
Nicolás-1400 <nicolasgarciasampedrodocampo@gmail.com>
2025-12-13 14:59:08 +0100

    Ahora si que está bien hecho
    Se podrían añadir algunas peticiones más

--- src/app.js
+++ src/app.js
@@ -4,18 +4,7 @@ const app = express();
 app.use(express.json());
 
 // Rutas
-app.use('/administradores', require('./routes/administradores'));
-/* app.use('/alumnos', require('./routes/alumnos'));
-app.use('/apuntes', require('./routes/apuntes'));
-app.use('/cursos', require('./routes/cursos'));
-app.use('/cursosAlumnos', require('./routes/cursosAlumnos'));
-app.use('/ejercicios', require('./routes/ejercicios'));
-app.use('/incidencias', require('./routes/incidencias'));
-app.use('/profesores', require('./routes/profesores'));
-app.use('/profesoresCursos', require('./routes/profesoresCursos'));
-app.use('/puntuacionesEjercicios', require('./routes/puntuacionesEjercicios'));
-app.use('/usuarios', require('./routes/usuarios'));
-app.use('/videos', require('./routes/videos')); */
+app.use('/', require('./routes/index.js'));
 
 // Inica el servidor
 app.listen(3000, () => console.log('Servidor ejecutándose en http://localhost:3000'));
