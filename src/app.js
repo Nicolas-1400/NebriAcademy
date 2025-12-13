e4401cb5241badb5117a6f0d571ff50bdf7f41e5
AlvaroSantosCalvo <aalvar.s.c@gmail.com>
2025-12-13 18:24:33 +0100

    Diagramas actualizados

--- src/app.js
+++ src/app.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-// Rutas
-app.get('/', (req, res) => {
-	res.send("Pagina principal de la API de NebriAcademy<br><br>Para poder ver los JSON de cada tabla con todos sus datos, poner en la URL localhost:3000/nombre de la tabla.<br>Para ver los JSON de un registro en concreto, poner en la URL localhost:3000/nombre de la tabla/id del registro.<br><br>Tablas disponibles:<br>/administradores<br>/alumnos<br>/apuntes<br>/cursos<br>/cursosalumnos<br>/ejercicios<br>/incidencias<br>/profesores<br>/profesorescursos<br>/puntuacionesejercicios<br>/usuarios<br>/videos");
-});
 
 // Rutas por recurso
 app.use('/', require('./routes/index'));
