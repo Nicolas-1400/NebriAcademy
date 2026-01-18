69d8d77d93eae933a2f8a56972bfe98645682695
AlvaroSantosCalvo <aalvar.s.c@gmail.com>
2026-01-18 20:01:55 +0100

    Login front

--- nebriacademy-frontend-2/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Home() {
-  return (
-    <h1>Estás en la página Home</h1>
-  )
-}
-
-export default Home
--- nebriacademy-frontend-2/src/pages/MasterClass.jsx
+++
@@ -1,8 +0,0 @@
-
-function MasterClass() {
-  return (
-    <h1>Estás en la página MasterClass</h1>
-  )
-}
-
-export default MasterClass
